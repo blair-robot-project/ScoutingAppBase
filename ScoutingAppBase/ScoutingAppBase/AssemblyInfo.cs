91399cc08b93bc5dcc19f01673ef8449cf8afdb1
ysthakur <45539777+ysthakur@users.noreply.github.com>
2022-05-14 20:57:41 -0400

    Add sample number entry, checkbox, and radio button group

--- ScoutingAppBase/ScoutingAppBase/AssemblyInfo.cs
+++ ScoutingAppBase/ScoutingAppBase/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.CommunityToolkit;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
--- ScoutingAppBase/ScoutingAppBase/MainPage.xaml.cs
+++ ScoutingAppBase/ScoutingAppBase/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.CommunityToolkit;
 
 namespace ScoutingAppBase
 {
@@ -13,6 +14,28 @@ namespace ScoutingAppBase
     public MainPage()
     {
       InitializeComponent();
+      Entry numInput = new Entry();
+      numInput.Behaviors.Add(new NumericValidationBehavior(1.0, 4.0, false));
+
+      CheckBox checkBox = new CheckBox();
+      checkBox.CheckedChanged += (sender, e) => { };
+
+      StackLayout radioButtons = createRadioButtons("Choose", new string[] { "foo", "bar" });
+
+      FieldsLayout.Children.Add(numInput);
+      FieldsLayout.Children.Add(checkBox);
+      FieldsLayout.Children.Add(radioButtons);
+    }
+
+    public StackLayout createRadioButtons(string label, string[] choices)
+    {
+      StackLayout layout = new StackLayout();
+      layout.Children.Add(new Label { Text = label });
+      foreach (string choice in choices)
+      {
+        layout.Children.Add(new RadioButton { Content = choice });
+      }
+      return layout;
     }
   }
 }
